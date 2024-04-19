0bf45a7e311d2c68f5a00600dabd581760d60985
FelipeGabrill <FelipeGabrill>
2024-04-19 10:33:15 -0300

    Order, Enum, relacionamento muitos-para-um

---
+++ src/main/java/com/dv/dscommerce/entities/Order.java
@@ -0,0 +1,40 @@
+package com.dv.dscommerce.entities;
+
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_order")
+@AllArgsConstructor
+@Getter
+@Setter
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
+
+	private OrderStatus status;
+
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+
+	public Order() {
+	}
+}
--- src/main/java/com/dv/dscommerce/entities/User.java
+++ src/main/java/com/dv/dscommerce/entities/User.java
@@ -1,11 +1,14 @@
 package com.dv.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,19 +18,32 @@ import lombok.Setter;
 @Table(name = "tb_user")
 @AllArgsConstructor
 @Getter
-@Setter
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Setter
 	private Long id;
 
+	@Setter
 	private String name;
+	
+	@Setter
 	private String email;
+	
+	@Setter
 	private String phone;
+	
+	@Setter
 	private LocalDate birthDate;
+	
+	@Setter
 	private String password;
 
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+	
 	public User() {
 	}
 }
