9be663aff091af547f6cde3294a74f84d23d3ba9
Felipe Gabriel <felipegabriel@MacBook-Air-de-Felipe.local>
2024-04-22 23:18:24 -0300

    Validação com Bean Validation

--- src/main/java/com/dv/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/dv/dscommerce/dto/ProductDTO.java
@@ -2,6 +2,9 @@ package com.dv.dscommerce.dto;
 
 import com.dv.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,9 +13,19 @@ import lombok.Getter;
 public class ProductDTO {
 
 	private Long id;
+	
+	@Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	
+	@Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String description;
+	
+	@Positive(message = "O preço deve ser posituivo")
 	private Double price;
+	
+	
 	private String imgUrl;
 
 	public ProductDTO(Product entity) {
