9a4bd540b729a581bfe99d65d90185195c93ce34
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-05-19 12:15:19 -0300

    Domain model User Role, seed

--- src/main/java/com/dv/dscommerce/entities/User.java
+++ src/main/java/com/dv/dscommerce/entities/User.java
@@ -2,23 +2,26 @@ package com.dv.dscommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table(name = "tb_user")
-@AllArgsConstructor
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
@@ -31,23 +34,52 @@ public class User {
 
 	@Setter
 	private String name;
-	
+
 	@Column(unique = true)
 	@Setter
 	private String email;
-	
+
 	@Setter
 	private String phone;
-	
+
 	@Setter
 	private LocalDate birthDate;
-	
+
 	@Setter
 	private String password;
 
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
-	
+
+	@ManyToMany
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
 	public User() {
+	}
+
+	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password,
+			List<Order> orders) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.birthDate = birthDate;
+		this.password = password;
+		this.orders = orders;
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
