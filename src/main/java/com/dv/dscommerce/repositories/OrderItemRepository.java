ee3e00a4f0da9e7a4902707b9af312e59404f5e0
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-05-20 21:32:56 -0300

    OrderItemDTO, imgUrl

--- src/main/java/com/dv/dscommerce/repositories/OrderItemRepository.java
+++ src/main/java/com/dv/dscommerce/repositories/OrderItemRepository.java
@@ -3,7 +3,6 @@ package com.dv.dscommerce.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.dv.dscommerce.entities.Order;
 import com.dv.dscommerce.entities.OrderItem;
 
 @Repository
